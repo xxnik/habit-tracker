a1a71cf9b01e1bcf87433ba406d0b1b1ecddb380
Delta Student <223nikhilanand@gmail.com>
2026-02-13 21:28:38 +0530

    updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const today = new Date();
 const day = today.getDate();
 const month = today.getMonth();
 const year = today.getFullYear();
+const daysInMonth = new Date(year, month + 1, 0).getDate();
 const months = [
   "January","February","March","April","May","June",
   "July","August","September","October","November","December"
@@ -23,8 +24,8 @@ for(let i=1;i<=days;i++){
       });
     }, 100);
     }
-     date.appendChild(div);
 }
+let habitToDelete = null;
 // Open popup
 document.querySelector(".btn").addEventListener("click", () => {
   document.getElementById("popup").style.display = "flex";
@@ -45,7 +46,8 @@ document.getElementById("addHabit").addEventListener("click", () => {
   document.getElementById("popup").style.display = "none";
   document.getElementById("habitInput").value = "";
 });
-
+const todayIndex=day-1;
+// let delbtn;
 // Function to create card
 function createHabitCard(name,save=false) {
   const container = document.querySelector(".habit-section");
@@ -57,75 +59,105 @@ card.innerHTML = `
   <div class="card-top">
     <div>
       <h2>${name}</h2>
-      <p>Streak: <span>0</span> days</p>
+      <p>Streak: <span >0</span> days</p>
     </div>
-    <div class="buttons">
-      <button>✔</button>
+      
+    <div >
+      <button class="del-button"><i class="fa-regular fa-trash-can"></i></i></button>
+      <button class="buttons">✔</button>
     </div>
   </div>
   <div class="grid"></div>
 `;
+let delbtn = card.querySelector(".del-button");
+
+delbtn.addEventListener("click", () => {
+  habitToDelete = { name, card };
+
+  document.getElementById("del-popup").style.display = "flex";
+});
+
+  // hide delete popup
+  document.getElementById("cancel").addEventListener("click", () => {
+  document.getElementById("del-popup").style.display = "none";  
+   habitToDelete = null;
+});
+
+
+
+  const streakSpan = card.querySelector(".card-top span");
 
   const grid = card.querySelector(".grid");
 
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <=daysInMonth; i++) {
     const box = document.createElement("div");
     box.className = "box";
     grid.appendChild(box);
   }
-  const monthKey = `${name}-${month}-${year}`;
-  let habitData = JSON.parse(localStorage.getItem(monthKey)) || {
-  lastDay: 0,
-  totalDays: 0,
-  doneDays: []
-  };
   
-  // Check if new day
-  if (habitData.lastDay !== day) {
-      const box = document.createElement("div");
-      box.className = "box";
-      grid.appendChild(box);
-      
-   }   
+ const monthKey = `${name}-${month}-${year}`;
+
+ 
+
+  let habitData = JSON.parse(localStorage.getItem(monthKey)) || {
+    lastDay: 0,
+    totalDays: 0,
+    doneDays: Array(daysInMonth).fill(0)
+  };
+    // delete ok button
+ 
+  // calculate streak
+const streak = streakCount(habitData.doneDays, todayIndex);
+console.log(`"streak:"${streak}`)
+streakSpan.textContent = streak;
+  
+
+  habitData.doneDays.forEach((val, i) => {
+    if (val === 1 && grid.children[i]) {
+      grid.children[i].classList.add("done");
+    }
+  });
+
    
-      const btn = card.querySelector(".buttons button");
-      btn.addEventListener("click", () => { 
-         const lastBox = grid.lastElementChild;
+  const btn = card.querySelector(".buttons");
+      btn.addEventListener("click", () => {
+      const todayIndex = day - 1;
 
-          if (!lastBox) return;
+      habitData.doneDays[todayIndex] =
+        habitData.doneDays[todayIndex] === 1 ? 0 : 1;
 
-          lastBox.classList.toggle("done");
-
-        if (!habitData.doneDays) habitData.doneDays = [];
-
-        if (lastBox.classList.contains("done")) {
-          habitData.doneDays.push(day);
-        } else {
-          habitData.doneDays =
-            habitData.doneDays.filter(d => d !== day);
-        }
-
-        localStorage.setItem(monthKey, JSON.stringify(habitData));
-      });
-
-      if (habitData.doneDays.includes(day)) {
-        const lastBox = grid.lastElementChild;
-        lastBox.classList.add("done");
-      }
-
-      habitData.lastDay = day;
-      habitData.totalDays += 1;
+      grid.children[todayIndex]
+          .classList.toggle("done");
+    const streak = streakCount(habitData.doneDays, todayIndex);
+    streakSpan.textContent = streak;
 
     localStorage.setItem(monthKey, JSON.stringify(habitData));
+  });
+  habitData.lastDay = day;
+  // habitData.totalDays += 1;
+
+  localStorage.setItem(monthKey, JSON.stringify(habitData));
  
  
   container.appendChild(card);
   if(save) saveHabit(name);
+
+  
   
 }
+ document.getElementById("ok").addEventListener("click", () => {
+  if (!habitToDelete) return;
+
+  removeHabit(habitToDelete.name);
+  habitToDelete.card.remove();
+  // localStorage.removeItem(monthKey);
+  document.getElementById("del-popup").style.display = "none";
+  habitToDelete = null;
+});
 // save habit name
+
 function saveHabit(name) {
-  let habits = JSON.parse(localStorage.getItem("habitList")) || [];
+  habits = JSON.parse(localStorage.getItem("habitList")) || [];
   habits.push(name);
   localStorage.setItem("habitList", JSON.stringify(habits));
 }
@@ -137,6 +169,31 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+function streakCount(doneDays,todayIndex){
+  let streak=0;
+  for(let i=todayIndex;i>=0;i--){
+    if(doneDays[i]===1){
+      streak++;
+    }
+    else{
+      break;
+    }
+  }
+  
+  return streak;  
+}
+
+function removeHabit(name) {
+  let habits = JSON.parse(localStorage.getItem("habitList")) || [];
+
+  // remove only that habit name
+  habits = habits.filter(habit => habit !== name);
+
+  localStorage.setItem("habitList", JSON.stringify(habits));
+  const monthKey = `${name}-${month}-${year}`;
+  localStorage.removeItem(monthKey);
+}
 
 
 
